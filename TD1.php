0232b55ebec09c9d5260e29a35bb13c7bf390536
u17033325 <lucas.urgenti@etu.univ-amu.fr>
2019-09-13 09:29:54 +0200

    TD1

---
+++ TD1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Titre de la page</title>
+</head>
+<body>
+    <?php
+        $var1 =  6;
+        $var2 = 1.3;
+        $var3 = 'Variable 3';
+        echo '<strong> Voici mon premier programme PHP <strong><br/>' . "\n";
+        echo 'C\est pas mal non ? <br/>';
+    ?>
+</body>
+</html>

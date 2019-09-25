21c70f9db6293a0ac5cd769a191ebc6c98835972
u17033325 <lucas.urgenti@etu.univ-amu.fr>
2019-09-25 15:04:28 +0200

    TD1

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<?php
+function start_page($title)
+{
+    echo ' <!DOCTYPE html> 
+                <html lang="fr">
+                    <head>
+                        <title>' . PHP_EOL . $title . '</title>
+                    </head>
+                <body>' . PHP_EOL
+    ;
+};
+?>
+
+<?php
+    start_page('titre');
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>

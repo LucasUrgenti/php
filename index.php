61cb06e6f5d30b538c48bd5b4aa7f191ba44c468
u17033325 <lucas.urgenti@etu.univ-amu.fr>
2019-09-25 14:43:39 +0200

    TD1

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         $var2 = 1.3;
         $var3 = 'Variable 3';
         echo '<strong> Voici mon premier programme PHP <strong><br/>' . "\n";
-        echo 'C\est pas mal non ? <br/>';
+        echo 'C\'est pas mal non ? <br/>';
     ?>
 </body>
 </html>

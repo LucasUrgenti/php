2efaac4b04e962e4d85df81156a801ab59e8972e
u17033325 <lucas.urgenti@etu.univ-amu.fr>
2019-09-25 15:57:48 +0200

    TD1

---
+++ Mutualisation.php
@@ -0,0 +1,25 @@
+<?php
+function start_page($title)
+{
+    echo ' <!DOCTYPE html> <html
+        lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body>' . PHP_EOL;
+};
+start_page('tittre');
+function start_page2()
+{
+    echo'<DOCTYPE html> <html> <head> <title> test </title></head>';
+}
+function end_page()
+{
+    echo'<body> <hr/><br/><strong>test</strong></body></br><hr/></body></html>';
+}
+start_page2();
+end_page();
+//date
+$test = date('l F d, y');
+$jour = date('d/m/Y', strtotime($test));
+echo  $jour . '<br/>';
+$jour = date('F d, Y , h : i a');
+echo  $jour . '</br>';
+?>
+ 

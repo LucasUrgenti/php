52c32e806eafac8c6ed2ed16512130ce090c7669
u17033325 <lucas.urgenti@etu.univ-amu.fr>
2019-10-02 16:10:15 +0200

    TD1

--- formulaire2.php
+++ formulaire2.php
@@ -8,18 +8,18 @@
         <input type="text" name="prenom" />
         <textarea name="message" rows="8" cols="45">
             Votre message ici.
-        </textarea>
+        </textarea><br />
         <select name="choix">
             <option value="choix1">Choix 1</option>
             <option value="choix2">Choix 2</option>
             <option value="choix3">Choix 3</option>
             <option value="choix4">Choix 4</option>
-        </select>
-        <input type="checkbox" name="case" id="case" /> <label for="case">Ma case à cocher</label>
+        </select><br />
+        <input type="checkbox" name="case" id="case" /> <label for="case">Ma case à cocher</label><br/>
         Aimez-vous les frites ?
         <input type="radio" name="frites" value="oui" id="oui" checked="checked" /> <label for="oui">Oui</label>
-        <input type="radio" name="frites" value="non" id="non" /> <label for="non">Non</label>
-        <input type="hidden" name="pseudo" value="Mateo21" />
+        <input type="radio" name="frites" value="non" id="non" /> <label for="non">Non</label><br/>
+        <input type="hidden" name="pseudo" value="Mateo21" /><br/>
         <input type="submit" value="Valider" />
     </p>
 </form>

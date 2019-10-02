e8c0531131eb56ff32cd53488e32731e551976f1
u17033325 <lucas.urgenti@etu.univ-amu.fr>
2019-10-02 14:36:30 +0200

    TD1

--- bonjour.php
+++ bonjour.php
@@ -1,9 +1,11 @@
 <?php
 if (isset($_GET['prenom']) AND isset($_GET['nom']) AND isset($_GET['repeter']))
 {
+    // 1 : On force la conversion en nombre entier
     $_GET['repeter'] = (int) $_GET['repeter'];
 
-    if($_GET['repeter'] >= 1 AND $_GET['repeter'] <= 100)
+    // 2 : Le nombre doit être compris entre 1 et 100
+    if ($_GET['repeter'] >= 1 AND $_GET['repeter'] <= 100)
     {
         for ($i = 0 ; $i < $_GET['repeter'] ; $i++)
         {
